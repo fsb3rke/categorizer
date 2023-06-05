b47e4497a9756631a6f3a89c0a36997e5d718a58
fsb3rke <berkeeavcii@gmail.com>
2023-06-05 03:33:18 +0300

    initator folders has been added

--- categorizer/main.py
+++ categorizer/main.py
@@ -8,10 +8,12 @@ current_dir = os.getcwd()
 # Create the categorizer folder in current directory
 os.mkdir(current_dir + "CATEGORIZER")
 
+# Create all categorizer init folders in current directory
+ext.win.init_folders()
+
 # Go to this directory
 current_dir = current_dir + "CATEGORIZER"
 
 # List all elements in directory
 file_list: list = ext.win.list_files(current_dir)
 
-print(current_dir, file_list)
